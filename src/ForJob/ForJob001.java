f1be801fcf83bea218d772fe8a8605de9fa2f83e
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-14 20:09:29 +0400

    Try to add my Salary

--- src/ForJob/ForJob001.java
+++ src/ForJob/ForJob001.java
@@ -2,4 +2,7 @@ package ForJob;
 
 public class ForJob001 {
      int mySalary = 1500000;
+     public  void salaryShow(){
+        System.out.println(mySalary);
+     }
 }
--- src/ForJob/Main.java
+++ src/ForJob/Main.java
@@ -3,5 +3,7 @@ package ForJob;
 public class Main {
     public static void main(String[] args){
         System.out.println("It is for Job");
+        ForJob001 mySal = new ForJob001();
+        mySal.salaryShow();
     }
 }
