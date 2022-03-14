e90cfc19987942cef9f5316119b672ff72b5876f
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-14 20:59:27 +0400

    Try to add my Salary

--- src/ForJob/ForJob001.java
+++ src/ForJob/ForJob001.java
@@ -2,6 +2,7 @@ package ForJob;
 
 public class ForJob001 {
      int mySalary = 1500000;
+     String currency = "dram";
      public  void salaryShow(){
         System.out.println(mySalary);
      }
