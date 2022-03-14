941f090ba95249c261ff47da52dd705105afb194
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-14 17:00:47 +0400

    Initial commit

---
+++ src/testing/Main.java
@@ -0,0 +1,9 @@
+package testing;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        OutDate.PrintInfo();
+    }
+}
---
+++ src/testing/OutDate.java
@@ -0,0 +1,7 @@
+package testing;
+
+public class OutDate {
+    public static void PrintInfo(){
+        System.out.println("Hi, my birthday at 16th March, I will become 31 years old");
+    }
+}
